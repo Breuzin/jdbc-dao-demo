90ef70d247488fc73d071bbb2938b79c6204b1bb
Breuzin <guimine.20@gmail.com>
2022-02-22 17:29:35 -0300

    Department Entity class created

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,17 @@
 package application;
 
+import java.sql.Connection;
+
+import model.entities.Department;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Connection conn = null;
+		Department obj = new Department(1, "Books");
+		System.out.println(obj);
+		
 	}
 
 }
