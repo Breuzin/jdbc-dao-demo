dd5134d3a734c7e115a274590a134eead01ae7c9
Breuzin <guimine.20@gmail.com>
2022-02-24 07:37:07 -0300

    Implementation of SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.sql.Connection;
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -16,6 +18,8 @@ public class Program {
 		
 		Seller seller = new Seller(21, "Kleber", "kleber@gmail.com", new Date(), 2000.00, obj);
 		System.out.println(seller);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 	}
 
 }
