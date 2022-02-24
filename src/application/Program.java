d4d713620c29fca103421a7d4afc51b4260bd4ac
Breuzin <guimine.20@gmail.com>
2022-02-24 11:22:10 -0300

    FindById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,14 +12,11 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		Connection conn = null;
-		Department obj = new Department(1, "Books");
-		System.out.println(obj);
-		
-		Seller seller = new Seller(21, "Kleber", "kleber@gmail.com", new Date(), 2000.00, obj);
-		System.out.println(seller);
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
+		
+		System.out.println(seller);
 	}
 
 }
