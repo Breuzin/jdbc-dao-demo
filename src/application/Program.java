b09a2fb92e98ca80e569224291297ca9b38d72bb
Breuzin <guimine.20@gmail.com>
2022-02-25 08:56:30 -0300

    DeleteById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -12,6 +13,7 @@ public class Program {
 
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println("=== TEST_1: test FindById ===");
@@ -28,16 +30,24 @@ public class Program {
 		list = sellerDao.findAll();
 		list.forEach(System.out::println);
 		
+		/*
 		System.out.println("\n=== TEST_4: test seller Insert ===");
 		Seller seller1 = new Seller(null, "Gregory", "greg@gmail.com", new Date(), 5004.00, department);
 		sellerDao.insert(seller1);
 		System.out.println("Inserted! New id = " + seller1.getId());
+		*/
 		
 		System.out.println("\n=== TEST_5: test seller Update ===");
 		seller = sellerDao.findById(8);
 		seller.setName("Frank Grove");
 		sellerDao.update(seller);
 		System.out.println("Update completed! ");
+		
+		System.out.println("\n=== TEST_6: test seller Delete ===");
+		System.out.println("Enter id for delete test: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Id deleted! ");
 	}
 
 }
